e814f148e5f009970a32627040ebf0c3298821ec
DanSher25 <dansher250@gmail.com>
2026-03-10 14:06:27 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 import { Fancybox } from "@fancyapps/ui";
 import IMask from "imask";
 import FlipDown from "./vendor/flipDown.js";
-// import Swiper from "swiper";
-// import { Navigation, EffectFade, Pagination, Autoplay } from "swiper/modules";
+import Swiper from "swiper";
+import { EffectFade, Pagination } from "swiper/modules";
 
 import "@fancyapps/ui/dist/fancybox.css";
-// import "swiper/css";
+import "swiper/css";
 // import "swiper/css/navigation";
-// import "swiper/css/effect-fade";
-// import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
 import "flipdown/dist/flipdown.css";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -46,6 +46,55 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  const initModelSlider = () => {
+    const sliders = document.querySelectorAll(".model__picture");
+    if (sliders.length < 0) return;
+
+    sliders.forEach((el) => {
+      const slider = el.querySelector(".model__slider");
+      const swiper = new Swiper(slider, {
+        modules: [Pagination, EffectFade],
+
+        slidesPerView: 1,
+        speed: 350,
+
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true,
+        },
+
+        allowTouchMove: false,
+      });
+
+      const colors = el.querySelectorAll(".model__color");
+
+      if (colors.length > 0) {
+        colors.forEach((color, index) => {
+          color.addEventListener("click", () => {
+            swiper.slideTo(index);
+            colors.forEach((c) => c.classList.remove("model__color--active"));
+            color.classList.add("model__color--active");
+          });
+        });
+      }
+    });
+  };
+
+  const initGallerySlider = () => {
+    const sliders = document.querySelectorAll(".model__picture");
+    if (sliders.length < 0) return;
+
+    sliders.forEach((el) => {
+      const slider = el.querySelector(".model__gallery-list");
+      const swiper = new Swiper(slider, {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      });
+    });
+  };
+
   initPhoneMask();
   initFlipDown();
+  initModelSlider();
+  initGallerySlider();
 });
